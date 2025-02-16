8d099bfbc0829c47ce1f0c19483dd012f68a4d78
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2025-02-15 22:57:55 -0300

    :bug: fix: ajustando validação de variáveis de ambiente para HOST e SMTP_PORT

--- src/env/index.ts
+++ src/env/index.ts
@@ -7,15 +7,15 @@ const envSchema = z.object({
     .enum(['development', 'production', 'test'])
     .default('development'),
   PORT: z.string().default('3000'),
-  HOST: z.string().default('localhost'),
+  HOST: z.string(),
   JWT_SECRET: z.string().default('secret'),
   SMTP_HOST: z.string(),
-  SMTP_PORT: z.number(),
+  SMTP_PORT: z.string(),
   SMTP_USER: z.string(),
   SMTP_PASSWORD: z.string(),
   SMTP_EMAIL: z.string(),
-  SMTP_GMAIL_APP_PASSWORD: z.string(),
-  SMTP_SERVICE: z.string(),
+  // SMTP_GMAIL_APP_PASSWORD: z.string(),
+  // SMTP_SERVICE: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
